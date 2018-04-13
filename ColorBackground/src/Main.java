eed45d649da4cf3676cfdd13065c373c21ac3d87
Dganit Uzan <Dganit Uzan@DESKTOP-EM31RFR>
2018-04-13 17:57:48 +0300

    all buttons in

--- ColorBackground/src/Main.java
+++ ColorBackground/src/Main.java
@@ -1,11 +1,15 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Main extends JFrame {
+public class Main {
+	
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		mainFrame frame = new mainFrame();
 	}
 	
-	public Main()
+	
 }
